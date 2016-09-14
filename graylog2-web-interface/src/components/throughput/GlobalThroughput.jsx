9be6f91ae940e66f356d09a3528219d0cf07f154
Pierre De Paepe <pdepaepe@oxalya.com>
2016-09-14 19:44:59 +0200

    Explicit throughput

--- graylog2-web-interface/src/components/throughput/GlobalThroughput.jsx
+++ graylog2-web-interface/src/components/throughput/GlobalThroughput.jsx
@@ -15,8 +15,8 @@ const GlobalThroughput = React.createClass({
     }
     return (
       <span>
-        In <strong className="total-throughput">{numeral(this.state.throughput.input).format('0,0')}</strong>{' '}
-        / Out <strong className="total-throughput">{numeral(this.state.throughput.output).format('0,0')}</strong> msg/s
+        Total in <strong className="total-throughput">{numeral(this.state.throughput.input).format('0,0')}</strong>{' '}
+        / out <strong className="total-throughput">{numeral(this.state.throughput.output).format('0,0')}</strong> msg/s
       </span>
     );
   },

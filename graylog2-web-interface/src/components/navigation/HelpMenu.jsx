60e8812c1fbd9600f63ba3015aa11de2170e8183
Pierre De Paepe <pdepaepe@oxalya.com>
2016-11-07 16:04:57 +0100

    No need of help

--- graylog2-web-interface/src/components/navigation/HelpMenu.jsx
+++ graylog2-web-interface/src/components/navigation/HelpMenu.jsx
@@ -11,14 +11,7 @@ const HelpMenu = React.createClass({
   },
   render() {
     return (
-      <NavDropdown navItem title="Help" id="user-menu-dropdown" active={this.props.active}>
-        <LinkContainer to={Routes.getting_started(true)}>
-          <MenuItem>Getting Started</MenuItem>
-        </LinkContainer>
-        <MenuItem href={DocsHelper.versionedDocsHomePage()} target="blank">
-          <i className="fa fa-external-link"></i> Documentation
-        </MenuItem>
-      </NavDropdown>
+      <span></span>
     );
   },
 });

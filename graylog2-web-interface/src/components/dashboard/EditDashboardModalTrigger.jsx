cf40385790098276263065ef7debd1ca662715a8
Pierre De Paepe <pierre.de-paepe@corp.ovh.com>
2018-07-19 14:52:26 +0200

    Avoid easy dashboard suppression

--- graylog2-web-interface/src/components/dashboard/EditDashboardModalTrigger.jsx
+++ graylog2-web-interface/src/components/dashboard/EditDashboardModalTrigger.jsx
@@ -19,22 +19,8 @@ const EditDashboardModalTrigger = React.createClass({
     this.refs.modal.open();
   },
   render() {
-    let triggerButtonContent;
-
-    if (this.props.children === undefined || this.props.children.length === 0) {
-      triggerButtonContent = this._isCreateModal() ? 'Create dashboard' : 'Edit dashboard';
-    } else {
-      triggerButtonContent = this.props.children;
-    }
-
     return (
-      <span>
-        <button onClick={this.openModal}
-                className={`btn ${this.props.buttonClass}`}>
-          {triggerButtonContent}
-        </button>
-        <EditDashboardModal ref="modal" {...this.props} />
-      </span>
+      <span></span>
     );
   },
 });

8745f8c7186a3a5bf937250f1b75a813e21dcb02
Pierre De Paepe <pierre.de-paepe@corp.ovh.com>
2019-01-07 17:19:47 +0100

    No need of help

--- graylog2-web-interface/src/components/support/ContactUs.jsx
+++ graylog2-web-interface/src/components/support/ContactUs.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 
-import DocumentationLink from 'components/support/DocumentationLink';
-import DocsHelper from 'util/DocsHelper';
-
 const ContactUs = React.createClass({
   render() {
     return (
@@ -12,22 +9,17 @@ const ContactUs = React.createClass({
           <div className="support-sources">
             <h2>Need help?</h2>
             <p>
-              Do not hesitate to consult the Graylog community if your questions are not answered in the{' '}
-              <DocumentationLink page={DocsHelper.PAGES.WELCOME} text="documentation" />.
+              Do not hesitate to consult the <a href="https://docs.ovh.com/gb/en/logs-data-platform/" target="_blank">Logs Data Platform documentation</a>.
             </p>
 
             <ul>
               <li>
                 <i className="fa fa-group" />&nbsp;
-                <a href="https://www.graylog.org/community-support/" target="_blank">Community support</a>
-              </li>
-              <li>
-                <i className="fa fa-github-alt" />&nbsp;
-                <a href="https://github.com/Graylog2/graylog2-server/issues" target="_blank">Issue tracker</a>
+                <a href="https://community.ovh.com/c/platform/data-platforms" target="_blank">Community support</a>
               </li>
               <li>
                 <i className="fa fa-heart" />&nbsp;
-                <a href="https://www.graylog.org/professional-support" target="_blank">Professional support</a>
+                <a href="https://www.ovh.com/support/" target="_blank">OVH support</a>
               </li>
             </ul>
           </div>

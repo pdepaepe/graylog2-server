18eea0427d1fe01ecd4a1975a79299674ae28cba
Pierre De Paepe <pierre.de-paepe@corp.ovh.com>
2019-02-27 18:20:07 +0100

    Minimal information on footer

---
+++ graylog2-web-interface/src/components/footer/ExtendedFooter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Reflux from 'reflux';
+import Version from 'util/Version';
+
+import StoreProvider from 'injection/StoreProvider';
+const SystemStore = StoreProvider.getStore('System');
+
+const Footer = React.createClass({
+  mixins: [Reflux.connect(SystemStore)],
+  componentDidMount() {
+    SystemStore.jvm().then(jvmInfo => this.setState({ jvm: jvmInfo }));
+  },
+  _isLoading() {
+    return !(this.state.system && this.state.jvm);
+  },
+  render() {
+    if (this._isLoading()) {
+      return (
+        <span></span>
+      );
+    }
+
+    return (
+      <span> on {this.state.system.hostname} ({this.state.jvm.info})</span>
+    );
+  },
+});
+
+export default ExtendedFooter;
--- graylog2-web-interface/src/components/layout/Footer.jsx
+++ graylog2-web-interface/src/components/layout/Footer.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import Reflux from 'reflux';
-import Version from 'util/Version';
 
-import StoreProvider from 'injection/StoreProvider';
-const SystemStore = StoreProvider.getStore('System');
+import Version from 'util/Version';
+import { IfPermitted } from 'components/common';
+import { ExtendedFooter } from 'components/footer';
 
 const Footer = React.createClass({
-  mixins: [Reflux.connect(SystemStore)],
-  componentDidMount() {
-    SystemStore.jvm().then(jvmInfo => this.setState({ jvm: jvmInfo }));
-  },
-  _isLoading() {
-    return !(this.state.system && this.state.jvm);
-  },
   render() {
-    if (this._isLoading()) {
-      return (
+    return (
         <div id="footer">
           Graylog {Version.getFullVersion()}
+          <IfPermitted permissions="notifications:read">
+            <ExtendedFooter />
+          </IfPermitted>
         </div>
-      );
-    }
-
-    return (
-      <div id="footer">
-        Graylog {this.state.system.version} on {this.state.system.hostname} ({this.state.jvm.info})
-      </div>
     );
   },
 });
+
 
 export default Footer;

8f082f62f46fae9a9201390b34291efa8ca57d1f
Pierre De Paepe <pierre.de-paepe@ovhcloud.com>
2020-12-01 22:20:27 +0100

    Minimal information on footer

---
+++ graylog2-web-interface/src/components/footer/ExtendedFooter.jsx
@@ -0,0 +1,55 @@
+// @flow strict
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import connect from 'stores/connect';
+import StoreProvider from 'injection/StoreProvider';
+
+const SystemStore = StoreProvider.getStore('System');
+
+type Props = {
+  system?: {
+    version: string,
+    hostname: string,
+  },
+};
+
+const ExtendedFooter = ({ system }: Props) => {
+  const [jvm, setJvm] = useState();
+
+  useEffect(() => {
+    let mounted = true;
+
+    SystemStore.jvm().then((jvmInfo) => {
+      if (mounted) {
+        setJvm(jvmInfo);
+      }
+    });
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  if (!(system && jvm)) {
+    return (
+      <>
+      </>
+    );
+  }
+  return (
+    <>
+      &nbsp;on {system.hostname} ({jvm.info})
+    </>
+  );
+};
+ExtendedFooter.propTypes = {
+  system: PropTypes.shape({
+    version: PropTypes.string,
+    hostname: PropTypes.string,
+  }),
+};
+ExtendedFooter.defaultProps = {
+  system: undefined,
+};
+export default connect(ExtendedFooter, { system: SystemStore }, ({ system: { system } = {} }) => ({ system }));
---
+++ graylog2-web-interface/src/components/footer/index.jsx
@@ -0,0 +1,1 @@
+export { default as ExtendedFooter } from './ExtendedFooter';
